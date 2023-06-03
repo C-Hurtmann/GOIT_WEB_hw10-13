0f626d19e3f9f702d6a39c7304561d01dceb11ca
C-Hurtmann <constantine2903@gmail.com>
2023-06-03 19:00:44 +0300

    add authentication

--- notes/users/views.py
+++ notes/users/views.py
@@ -1,3 +1,6 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from . import forms
@@ -15,3 +18,22 @@ def signupuser(request):
         else:
             return render(request, 'users/signup.html', context={'form': form})
     return render(request, 'users/signup.html', context={'form': forms.RegisterForm()})
+
+def loginuser(request):
+    if request.is_authenticated():
+        return redirect(to='noteapp:main')
+    
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['ussername'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, "Username or password didn\'t match")
+            return redirect(to='users:login')
+        login(request, user)
+        return redirect(to='noteapp:main')
+    
+    return render(request, 'users/login.html', context={'form': forms.LoginForm})
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='noteapp:main')
