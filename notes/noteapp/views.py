6f823fe798a5f67ad02635e88397c04b67d44c9d
C-Hurtmann <constantine2903@gmail.com>
2023-06-03 09:02:41 +0300

    add tag and note form

--- notes/noteapp/views.py
+++ notes/noteapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . import forms
+from . import models
 # Create your views here.
 def main(request):
     return render(request, 'noteapp/index.html')
@@ -9,9 +10,27 @@ def tag(request):
         form = forms.TagForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(to='moteapp:main')
+            return redirect(to='noteapp:main')
         else:
             return render(request, 'noteapp/tag.html', {'form': form})
     
     return render(request, 'noteapp/tag.html', {'form': forms.TagForm()})
+
+def note(request):
+    tags = models.Tag.objects.all()
+    
+    if request.method == 'POST':
+        form = forms.NoteForm(request.POST)
+        if form.is_valid():
+            new_note = form.save()
+            choise_tags = models.Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choise_tags.iterator():
+                new_note.tags.add(tag)
+            
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'noteapp/note.html', {'tags': tags, 'form': form})
+    return render(request, 'noteapp/note.html', {"tags": tags, 'form': forms.NoteForm()})
+
+
     
