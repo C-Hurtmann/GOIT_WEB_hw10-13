7df8a38203c1c96b87ef77641b4fda393fa08d60
C-Hurtmann <constantine2903@gmail.com>
2023-07-02 21:38:34 +0300

    add reset password workspace

--- quotes/users/urls.py
+++ quotes/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import PasswordResetDoneView, PasswordResetCompleteView, PasswordResetConfirmView
 from django.urls import path
 from . import views
 
@@ -7,4 +8,13 @@ urlpatterns = [
     path('signup/', views.signupuser, name='signup'),
     path('login/', views.loginuser, name='login'),
     path('logout/', views.logoutuser, name='logout'),
+    path('reset-password/', views.ResetPasswordView.as_view(), name='reset-password'), # first step
+    path('reset-password/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='reset-password-done'), # second step. Request for password reset has been sent
+    path('reset-password/confirm/<uidb64>/<token>',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                          success_url='users/password_reset_complete.html'),
+         name='reset-password-confirm'),
+    path('reset-password/complete', PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='reset-password-complete')
 ]
