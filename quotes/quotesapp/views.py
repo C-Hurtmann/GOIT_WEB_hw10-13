038b967ff775608cd6803eed0a8c43c1de3aa5a3
C-Hurtmann <constantine2903@gmail.com>
2023-08-09 19:28:59 +0300

    change views to CBV

--- quotes/quotesapp/views.py
+++ quotes/quotesapp/views.py
@@ -1,8 +1,7 @@
 from typing import Any
-from django.core.paginator import Paginator
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.query import QuerySet
-from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, CreateView, DetailView
 from django.urls import reverse_lazy
 
@@ -10,18 +9,12 @@ from .forms import TagForm, AuthorForm, QuoteForm
 from .models import Tag, Author, Quote
 from .utils import SetUserToModelMixin
 # Create your views here.
-def main(request):
-    quotes = Quote.objects.all()
-    
-    paginator = Paginator(quotes, 5)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'quotesapp/index.html', {'page_obj': page_obj, 'quotes': quotes})
 
 
 class QuotesHome(ListView):
     model = Quote
     template_name = 'quotesapp/index.html'
+    paginate_by = 5
     
     def get_queryset(self) -> QuerySet[Any]:
         return super().get_queryset()
