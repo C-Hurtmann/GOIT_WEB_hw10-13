4c544462f9cc856a7a2c3ca74c2b2806e039491d
C-Hurtmann <constantine2903@gmail.com>
2023-08-10 18:38:00 +0300

    add dockerfile and turned off debug mode

--- quotes/quotes/settings.py
+++ quotes/quotes/settings.py
@@ -25,9 +25,9 @@ env.read_env(BASE_DIR.parent / '.env')
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,8 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 # Default primary key field type
--- quotes/quotes/urls.py
+++ quotes/quotes/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path, include
+from django.views.static import serve
+from django.urls import path, include, re_path
 
 
 urlpatterns = [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),           
     path('admin/', admin.site.urls),
     path('', include('quotesapp.urls')),
     path('users/', include('users.urls')),
